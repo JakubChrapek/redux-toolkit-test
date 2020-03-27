98fbc2f09e66bac89d144f7c3e3b382dceffffe2
chrapekuba@gmail.com <chrapekuba@gmail.com>
2020-03-27 17:45:31 +0100

    Redux Toolkit basic quiz ready

--- src/features/quiz/Quiz.js
+++ src/features/quiz/Quiz.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { 
+  selectActiveQuestionNumber,
+  selectQuestions,
+  selectFinished,
+  selectMaxPoints,
+  selectScore,
+  next,
+  prev,
+  toggle,
+  finish,
+} from './quizSlice';
+import { Button } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const Quiz = () => {
-  return <div>Quiz</div>
+  const currentQuestionNumber = useSelector(selectActiveQuestionNumber);
+  const questions = useSelector(selectQuestions);
+  const finished = useSelector(selectFinished);
+  const dispatch = useDispatch();
+  const maxPoints = useSelector(selectMaxPoints);
+  const score = useSelector(selectScore);
+  
+  const handleNext = () => {
+    if(finished) return
+    if(questions[currentQuestionNumber + 1]) {
+      dispatch(next());
+    } else {
+      dispatch(finish());
+    }
+  };
+
+  const handleToggle = (answerIndex) => {
+    dispatch(toggle(answerIndex));
+  };
+
+  return <div>
+    {finished ? 
+    <p>Podsumowanie: {score} / {maxPoints}</p>
+    
+    :
+      <>
+        <p>{currentQuestionNumber+1} / {questions.length}</p>
+        <p>{questions[currentQuestionNumber].content}</p>
+        <List>
+          {questions[currentQuestionNumber].answers.map((answer, index) =>
+            <ListItem key={`checkbox-${index}`} dense button onClick={() => handleToggle(index)}>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={answer.checked}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': answer.id }}
+                />
+              </ListItemIcon>
+              <ListItemText id={answer.id} primary={answer.content} />
+            </ListItem>
+          )}
+        </List>
+
+        <Button disabled={currentQuestionNumber === 0}onClick={() => dispatch(prev())}>Prev</Button>
+        <Button onClick={handleNext}>
+          {currentQuestionNumber === questions.length - 1 ? `Zakończ` : `Next`}
+        </Button>
+      </>
+    }
+  </div>
 }
 
 export default Quiz
--- src/features/quiz/quizSlice.js
+++ src/features/quiz/quizSlice.js
@@ -3,14 +3,75 @@ import { createSlice } from '@reduxjs/toolkit';
 export const slice = createSlice({
   name: 'quiz',
   initialState: {
+    activeQuestion: 0,
+    finished: false,
+    score: 0,
     questions: [
-      { content: "How to start with Redis?" },
-      { content: "How to start with Firebase?" },
+      { 
+        content: "How to start with Redis?",
+        answers: [
+          { id: 1, content: 'Just start! Done is better than perfect.', checked: false, points: 1 },
+          { id: 2, content: 'Read all the documentation!', checked: false, points: 0 },
+          { id: 3, content: 'Google it.', checked: false, points: 1 },
+        ]
+      
+      },
+      {
+        content: "How to start with Firebase?",
+        answers: [
+          { id: 1, content: 'Get in touch with NetNinja tutorial.', checked: false, points: 0 },
+          { id: 2, content: 'Docs 4the win!', checked: false, points: 1 },
+          { id: 3, content: 'Create a mini project.', checked: false, points: 0 },
+        ]
+      },
+      {
+        content: "How to start with Redux-Toolkit?",
+        answers: [
+          { id: 1, content: 'Chmaro knows a little bit 😬.', checked: false, points: 1 },
+          { id: 2, content: 'Read Redux docs ↔ Read Redux Toolkit docs', checked: false, points: 1 },
+          { id: 3, content: 'Create a small project.', checked: false, points: 1 },
+        ]
+      },
     ]
   },
   reducers: {
-
+    next: state => {
+      state.activeQuestion += 1;
+    },
+    prev: state => {
+      state.activeQuestion -= 1;
+    },
+    toggle: (state, action) => {
+      const question = state.questions[state.activeQuestion];
+      question.answers[action.payload].checked = !question.answers[action.payload].checked;
+    },
+    finish: state => {
+      state.finished = true;
+    }
   },
 });
+
+//REDUCERS TO DISPATCH
+export const { next, prev, toggle, finish } = slice.actions;
+
+// SELECTORS
+export const selectQuestions = state => state.quiz.questions;
+export const selectActiveQuestionNumber = state => state.quiz.activeQuestion;
+export const selectFinished = state => state.quiz.finished;
+export const selectMaxPoints = state => {
+  let maxPoints = 0;
+  state.quiz.questions.forEach(question => {
+    maxPoints += question.answers.filter(answer => answer.points > 0).length
+  });
+  return maxPoints;
+}
+export const selectScore = state => {
+  let score = 0;
+  state.quiz.questions.forEach(question => {
+    score += question.answers
+    .filter(answer => answer.checked && answer.points > 0).length;
+  });
+  return score;
+}
 
 export default slice.reducer;
